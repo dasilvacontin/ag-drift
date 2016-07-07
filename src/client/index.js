5032f26938a93ae77d971687dea4073dd5e5740e
David da Silva <dasilvacontin@gmail.com>
2016-07-07 21:19:29 +0200

    Improve thruster effects

--- src/client/index.js
+++ src/client/index.js
@@ -9,11 +9,11 @@ const clone = require('clone')
 const renderer = new PIXI.autoDetectRenderer(
   window.innerWidth,
   window.innerHeight,
-  { backgroundColor: 0xAAAAFF })
+  { backgroundColor: 0x3F51B5 })
 document.body.appendChild(renderer.view)
 
 const stage = global.stage = new PIXI.Container()
-stage.scale = { x: 12, y: 12 }
+stage.scale = { x: 20, y: 20 }
 
 function onResize () {
   const width = window.innerWidth
@@ -25,6 +25,8 @@ function onResize () {
 onResize()
 window.addEventListener('resize', onResize)
 
+const SMALL_THRUSTER_WIDTH = 0.5
+const SMALL_THRUSTER_LONG = 0.4
 const cars = []
 function addCar () {
   const body = new p2.Body({
@@ -37,37 +39,59 @@ function addCar () {
   body.addShape(shape)
   world.addBody(body)
 
+  const color = 0xFFFFFF * Math.random()
   const sprite = new PIXI.Graphics()
   sprite.pivot = { x: 1, y: 1 }
-  sprite.beginFill(0xFFFFFF * Math.random())
+  sprite.beginFill(color)
   sprite.drawRect(0, 0, 2, 2)
   sprite.endFill()
+  // windshield
+  sprite.moveTo(0.35, 0.1)
+  sprite.fillAlpha = 0.8
+  sprite.beginFill(0xFFFFFF)
+  sprite.lineTo(1.65, 0.1)
+  sprite.lineTo(1.3, 0.9)
+  sprite.lineTo(0.7, 0.9)
+  sprite.endFill()
+
+  sprite.beginFill(0x000000)
+  sprite.fillAlpha = 0.1
+  sprite.moveTo(0.7, 0.9)
+  sprite.lineTo(1.3, 0.9)
+  sprite.lineTo(1.5, 2)
+  sprite.lineTo(0.5, 2)
+  sprite.endFill()
+  stage.addChild(sprite)
 
   const fire = new PIXI.Graphics()
-  fire.position = { x: 1, y: 1.75 }
-  fire.beginFill(0xFF0000)
+  fire.position = new PIXI.Point(1, 1.75)
+  fire.beginFill(0xFFFFFF)
   fire.drawRect(-0.5, 0, 1, 0.5)
   fire.endFill()
   sprite.addChild(fire)
   sprite.fire = fire
 
   const fireLeft = new PIXI.Graphics()
-  fireLeft.beginFill(0xFF0000)
-  fireLeft.drawRect(-0.5, 0.75, 0.5, 0.5)
+  fireLeft.beginFill(0xFFFFFF)
+  fireLeft.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
   fireLeft.endFill()
+  fireLeft.position = new PIXI.Point(0, 1)
+  fireLeft.rotation = Math.PI / 2
   fireLeft.visible = false
+  fireLeft.alpha = 0.5
   sprite.addChild(fireLeft)
   sprite.fireLeft = fireLeft
 
   const fireRight = new PIXI.Graphics()
-  fireRight.beginFill(0xFF0000)
-  fireRight.drawRect(2, 0.75, 0.5, 0.5)
+  fireRight.beginFill(0xFFFFFF)
+  fireRight.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
   fireRight.endFill()
+  fireRight.position = new PIXI.Point(2, 1)
+  fireRight.rotation = -Math.PI / 2
   fireRight.visible = false
+  fireRight.alpha = 0.5
   sprite.addChild(fireRight)
   sprite.fireRight = fireRight
-
-  stage.addChild(sprite)
 
   const username = ' ' || prompt(`Username for Player #${cars.length + 1}:`)
   const car = { body, shape, sprite, username }
@@ -90,24 +114,30 @@ const CELL_EDGE = 10
 const HALF_EDGE = CELL_EDGE / 2
 map.forEach((row, i) => {
   row.split('').forEach((cell, j) => {
-    if (cell !== '1') return
-    const cellShape = new p2.Box({ width: CELL_EDGE, height: CELL_EDGE })
-    const cellBody = new p2.Body({
-      mass: 0,
-      position: [j * CELL_EDGE, i * CELL_EDGE]
-    })
-    cellBody.addShape(cellShape)
-    world.addBody(cellBody)
+    if (cell === '1') {
+      const cellShape = new p2.Box({ width: CELL_EDGE, height: CELL_EDGE })
+      const cellBody = new p2.Body({
+        mass: 0,
+        position: [j * CELL_EDGE, i * CELL_EDGE]
+      })
+      cellBody.addShape(cellShape)
+      world.addBody(cellBody)
+    }
+
     const sprite = new PIXI.Graphics()
-    sprite.beginFill(0x3F51B5)
+    sprite.beginFill(cell === '1' ? 0x3F51B5 : 0xAAAAFF)
     sprite.drawRect(0, 0, 10, 10)
     sprite.endFill()
-    sprite.position = { x: j * CELL_EDGE - HALF_EDGE, y: i * CELL_EDGE - HALF_EDGE }
+    sprite.position = new PIXI.Point(j * CELL_EDGE - HALF_EDGE, i * CELL_EDGE - HALF_EDGE)
     stage.addChild(sprite)
   })
 })
 
 function onAssetsLoaded () {
+  addCar()
+  addCar()
+  addCar()
+  addCar()
   addCar()
   addCar()
   loop()
@@ -127,8 +157,8 @@ function padIsKeyDown (gamepad, key) {
   const axes = gamepad.axes.map(Math.round)
 
   switch (key) {
-    case kbd.RIGHT_ARROW: return axes[0] === 1 || buttons[15].pressed
-    case kbd.LEFT_ARROW: return axes[0] === -1 || buttons[14].pressed
+    case kbd.RIGHT_ARROW: return axes[0] === 1 || buttons[15] && buttons[15].pressed
+    case kbd.LEFT_ARROW: return axes[0] === -1 || buttons[14] && buttons[14].pressed
     case kbd.UP_ARROW: return axes[1] === -1
     case kbd.DOWN_ARROW: return axes[1] === 1
     case 'q': return buttons[4].pressed
@@ -136,6 +166,9 @@ function padIsKeyDown (gamepad, key) {
     default: throw new Error('Unsupported key')
   }
 }
+
+global.FIRE_HIGH = 0x007AFF
+global.FIRE_LOW = 0x00FFD4
 
 const meter = new FPSMeter()
 function loop () {
@@ -146,7 +179,8 @@ function loop () {
 
   if (gamepads.length > 0) {
     cars.forEach((car, i) => {
-      const { body } = car
+      const { body, sprite } = car
+      const { fire, fireLeft, fireRight } = sprite
       const gamepad = gamepads[i]
       const nowLeft = padIsKeyDown(gamepad, kbd.LEFT_ARROW)
       const nowRight = padIsKeyDown(gamepad, kbd.RIGHT_ARROW)
@@ -158,17 +192,29 @@ function loop () {
       if (nowRight && !oldRight) body.angle += Math.PI / 2
       body.applyForceLocal([0, -force]) // thrust
 
+      // thruster animation
+      const thrusters = [fire, fireLeft, fireRight]
+      thrusters.forEach((thruster) => {
+        thruster.scale.x = 1 - Math.random() * 0.5
+        thruster.scale.y = Math.random() * 1 + 1
+        thruster.tint = Math.random() < 0.5
+          ? global.FIRE_HIGH
+          : global.FIRE_LOW
+      })
+
       const leftThrusterOn = padIsKeyDown(gamepad, 'q')
       if (leftThrusterOn) body.applyForceLocal([force, 0])
-      car.sprite.fireLeft.visible = leftThrusterOn
+      fireLeft.visible = leftThrusterOn
 
       const rightThrusterOn = padIsKeyDown(gamepad, 'e')
       if (rightThrusterOn) body.applyForceLocal([-force, 0])
-      car.sprite.fireRight.visible = rightThrusterOn
+      fireRight.visible = rightThrusterOn
 
+      // air drag
       vec2.scale(body.velocity, body.velocity, 0.99)
     })
   }
+
   oldGamepads = Array.from(gamepads).map((gamepad) => {
     if (gamepad == null) return null
     const { axes, buttons } = gamepad
@@ -185,19 +231,13 @@ function loop () {
 
   cars.forEach((car) => {
     const { body, sprite } = car
-    sprite.position = { x: body.position[0], y: body.position[1] }
+    sprite.position = new PIXI.Point(body.position[0], body.position[1])
     sprite.rotation = body.angle
-    sprite.fire.scale.x = 1 - Math.random() * 0.5
-    sprite.fire.scale.y = Math.random() * 1 + 1
-    sprite.fire.tint = Math.random() < 0.5
-      ? 0xFF0000
-      : 0xFFFFFF
   })
 
-  stage.position = {
-    x: window.innerWidth / 2 - cars[0].sprite.position.x * stage.scale.x,
-    y: window.innerHeight / 2 - cars[0].sprite.position.y * stage.scale.y
-  }
+  stage.position = new PIXI.Point(
+    window.innerWidth / 2 - cars[0].sprite.position.x * stage.scale.x,
+    window.innerHeight / 2 - cars[0].sprite.position.y * stage.scale.y)
   renderer.render(stage)
   meter.tick()
 }
