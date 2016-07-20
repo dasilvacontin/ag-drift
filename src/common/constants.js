9a21e90d056b6999a30bc795d5d2fd3a0dab268e
David da Silva <dasilvacontin@gmail.com>
2016-07-21 01:21:43 +0200

    logic steps back to normal kthx

--- src/common/constants.js
+++ src/common/constants.js
@@ -28,7 +28,7 @@ const constants = {
   InvalidTurnError: subError('InvalidTurnError')
 }
 
-constants.TIME_STEP = 1000 / 10
+constants.TIME_STEP = 1000 / 60
 constants.TURN_MAX_DELAY = Math.ceil(500 / constants.TIME_STEP)
 
 module.exports = constants
