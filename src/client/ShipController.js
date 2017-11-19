5d6c22d912324c7e0f22db2955492969382f8a54
David da Silva <dasilvacontin@gmail.com>
2017-11-19 18:48:35 +0100

    Make ships have dual side boosters instead

--- src/client/ShipController.js
+++ src/client/ShipController.js
@@ -29,8 +29,10 @@ function isSoundPaused (sound, nodeId) {
 class ShipController {
   sprite: PIXI.DisplayObject
   color: number
-  leftFire: PIXI.DisplayObject
-  rightFire: PIXI.DisplayObject
+  frontLeftFire: PIXI.DisplayObject
+  rearLeftFire: PIXI.DisplayObject
+  frontRightFire: PIXI.DisplayObject
+  rearRightFire: PIXI.DisplayObject
   mainFire: PIXI.DisplayObject
   engineSoundId: ?number
 
@@ -49,17 +51,6 @@ class ShipController {
     sprite.lineTo(2, 2)
     sprite.lineTo(0, 2)
     sprite.endFill()
-
-    /*
-    // windshield
-    sprite.moveTo(0.35, 0.1)
-    sprite.beginFill(0xFFFFFF)
-    sprite.fillAlpha = 0.75
-    sprite.lineTo(1.65, 0.1)
-    sprite.lineTo(1.3, 0.9)
-    sprite.lineTo(0.7, 0.9)
-    sprite.endFill()
-    */
 
     // chasis peak
     sprite.beginFill(0x000000)
@@ -80,29 +71,52 @@ class ShipController {
     sprite.addChild(mainFire)
     this.mainFire = sprite.mainFire = mainFire
 
-    // left thruster
-    const leftFire = new PIXI.Graphics()
-    leftFire.beginFill(0xFFFFFF)
-    leftFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
-    leftFire.endFill()
-    leftFire.position = new PIXI.Point(0, 1)
-    leftFire.rotation = Math.PI / 2
-    leftFire.visible = false
-    leftFire.alpha = 0.5
-    sprite.addChild(leftFire)
-    this.leftFire = sprite.leftFire = leftFire
+    const frontThrusterHeight = 0.4
+    const rearThrusterHeight = 1.5
 
-    // right thruster
-    const rightFire = new PIXI.Graphics()
-    rightFire.beginFill(0xFFFFFF)
-    rightFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
-    rightFire.endFill()
-    rightFire.position = new PIXI.Point(2, 1)
-    rightFire.rotation = -Math.PI / 2
-    rightFire.visible = false
-    rightFire.alpha = 0.5
-    sprite.addChild(rightFire)
-    this.rightFire = sprite.rightFire = rightFire
+    const frontLeftFire = new PIXI.Graphics()
+    frontLeftFire.beginFill(0xFFFFFF)
+    frontLeftFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
+    frontLeftFire.endFill()
+    frontLeftFire.position = new PIXI.Point(0.1, frontThrusterHeight)
+    frontLeftFire.rotation = Math.PI / 2
+    frontLeftFire.visible = false
+    frontLeftFire.alpha = 0.5
+    sprite.addChild(frontLeftFire)
+    this.frontLeftFire = sprite.frontLeftFire = frontLeftFire
+
+    const rearLeftFire = new PIXI.Graphics()
+    rearLeftFire.beginFill(0xFFFFFF)
+    rearLeftFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
+    rearLeftFire.endFill()
+    rearLeftFire.position = new PIXI.Point(0, rearThrusterHeight)
+    rearLeftFire.rotation = Math.PI / 2
+    rearLeftFire.visible = false
+    rearLeftFire.alpha = 0.5
+    sprite.addChild(rearLeftFire)
+    this.rearLeftFire = sprite.rearLeftFire = rearLeftFire
+
+    const frontRightFire = new PIXI.Graphics()
+    frontRightFire.beginFill(0xFFFFFF)
+    frontRightFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
+    frontRightFire.endFill()
+    frontRightFire.position = new PIXI.Point(1.9, frontThrusterHeight)
+    frontRightFire.rotation = -Math.PI / 2
+    frontRightFire.visible = false
+    frontRightFire.alpha = 0.5
+    sprite.addChild(frontRightFire)
+    this.frontRightFire = sprite.frontRightFire = frontRightFire
+
+    const rearRightFire = new PIXI.Graphics()
+    rearRightFire.beginFill(0xFFFFFF)
+    rearRightFire.drawRect(-SMALL_THRUSTER_WIDTH / 2, -0.1, SMALL_THRUSTER_WIDTH, SMALL_THRUSTER_LONG)
+    rearRightFire.endFill()
+    rearRightFire.position = new PIXI.Point(2, rearThrusterHeight)
+    rearRightFire.rotation = -Math.PI / 2
+    rearRightFire.visible = false
+    rearRightFire.alpha = 0.5
+    sprite.addChild(rearRightFire)
+    this.rearRightFire = sprite.rearRightFire = rearRightFire
   }
 
   update (ship: Ship) {
@@ -119,7 +133,13 @@ class ShipController {
     if (input.gas) boost = input.boost ? 2 : 1
 
     // thruster animation
-    const thrusters = [this.mainFire, this.leftFire, this.rightFire]
+    const thrusters = [
+      this.mainFire,
+      this.frontLeftFire,
+      this.rearLeftFire,
+      this.frontRightFire,
+      this.rearRightFire
+    ]
     thrusters.forEach((thruster, k) => {
       // random flame shiverring
       thruster.scale.x = 1 - Math.random() * 0.5
@@ -135,8 +155,10 @@ class ShipController {
     })
 
     // active / deactivate support thrusters' sprites
-    this.leftFire.visible = input.leanR
-    this.rightFire.visible = input.leanL
+    this.frontLeftFire.visible = input.leanR || input.turnR
+    this.rearLeftFire.visible = input.leanR || input.turnL
+    this.frontRightFire.visible = input.leanL || input.turnL
+    this.rearRightFire.visible = input.leanL || input.turnR
     let { engineSoundId } = this
 
     if (boost > 0 || input.leanL || input.leanR) {
